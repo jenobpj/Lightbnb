a17e614a1036e79a01516658ad4ee5229a651d92
jenobpj <jenobgit@gmail.com>
2021-10-13 20:40:35 -0400

    error occured and file deleted

--- seeds/01_seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users(name,email,password)
-VALUES('Eva Stanley','sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-
-INSERT INTO users(name,email,password)
-VALUES('Louisa Meyer','acksonrose@hotmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-
-INSERT INTO users(name,email,password)
-VALUES('Dominic Parks','victoriablackwell@outlook.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
